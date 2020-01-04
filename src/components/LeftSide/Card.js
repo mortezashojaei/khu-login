f022f489459f321a1c1aa3547b837e7472f34eaf
morteza shojaei <mshojaei179@gmail.com>
2020-01-04 21:47:22 +0330

    add basic tab

---
+++ src/components/LeftSide/Card.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Card = () => {
+    return ( 
+        <div className="card m-t-10">نحوه ثبت نام در سیستم تغذیه</div>
+     );
+}
+ 
+export default Card;
--- src/components/LeftSide/index.js
+++ src/components/LeftSide/index.js
@@ -1,7 +1,15 @@
 import React from "react";
-
+import Card from "./Card";
+import Header from "./Header";
 const LeftSide = () => {
-  return <div className="login-more login-more-background"></div>;
+  return (
+    <div className="login-more">
+      <Header />
+      <div className="cards-wrapper">
+       
+      </div>
+    </div>
+  );
 };
 
 export default LeftSide;
