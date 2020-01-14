9b8933182af11cb2a711f028b659ef94de88df7c
morteza shojaei <mshojaei179@gmail.com>
2020-01-15 03:14:49 +0330

    handle card ui

--- src/components/LeftSide/index.js
+++ src/components/LeftSide/index.js
@@ -1,16 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Tabs from "./Tabs";
 import Cards from "./Cards";
+import axios from "axios";
 const LeftSide = () => {
   const [tabsIndex, setTabsIndex] = useState(1);
-  const [links, setLinks] = useState([1, 2, 3, 4, 5, 6]);
-  const [news, setNews] = useState([1, 2]);
+  const [section_one, setSectionOne] = useState("");
+  const [section_two, setSectionTwo] = useState("");
 
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/main-page-data")
+      .then(response => {
+        setSectionOne(response.data.section_one);
+        setSectionTwo(response.data.section_two);
+      })
+      .catch(error => {
+        alert("error");
+      });
+  }, []);
   return (
     <div className="login-more">
       <div className="sizefull flex-col-c ">
-        <Tabs tabsIndex={tabsIndex} setTabsIndex={setTabsIndex} />
-        <Cards items={tabsIndex ? links : news} />
+        <Tabs
+          titleOne={section_one.title || ""}
+          titleTwo={section_two.title || ""}
+          tabsIndex={tabsIndex}
+          setTabsIndex={setTabsIndex}
+        />
+        <Cards
+          items={tabsIndex ? section_one.data || [] : section_two.data || []}
+        />
       </div>
     </div>
   );
